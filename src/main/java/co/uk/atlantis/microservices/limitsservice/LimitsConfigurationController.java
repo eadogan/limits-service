357547e676ac5fdd9587a64d6286137b755daa76
eadogan <e.ahmetdogan@gmail.com>
2022-01-18 15:50:07 +0000

    Enhance service to get configuration from application properties

--- src/main/java/co/uk/atlantis/microservices/limitsservice/LimitsConfigurationController.java
+++ src/main/java/co/uk/atlantis/microservices/limitsservice/LimitsConfigurationController.java
@@ -7,8 +7,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LimitsConfigurationController {
 
+    private Configuration configuration;
+
+    public LimitsConfigurationController(Configuration configuration) {
+        this.configuration = configuration;
+    }
+
     @GetMapping("/limits")
     public LimitConfiguration retrieveLimitFromConfiguration() {
-        return new LimitConfiguration(1000, 1);
+        return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
     }
 }
