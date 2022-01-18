54e6efe297122f3588acfcae4dbd21ba41a4bb48
eadogan <e.ahmetdogan@gmail.com>
2022-01-18 13:42:29 +0000

    Create Skeleton of Project

---
+++ src/test/java/co/uk/atlantis/microservices/limitsservice/LimitsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.uk.atlantis.microservices.limitsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
